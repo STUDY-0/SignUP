35f312e57f4ae9267511b57689ccfeb71f4b14c1
Yu Jung Eun <0hh.0553@gmail.com>
2023-04-17 09:03:37 +0900

    버튼 변경, sign up .container margin-top: 130px

--- hello-react/src/Signup.js
+++ hello-react/src/Signup.js
@@ -5,33 +5,31 @@ import "./css/base.css"
 
 function Signup() {
     return (
-        <body>
-            <div>
-                <div class="title">sign up</div>
-                <div class="flex">
-                    <div class="input-div">
-                        <label class="input-label">아이디</label>
-                        <input type="text"/>
-                    </div>
-                    <div class="input-div">
-                        <label class="input-label">비밀번호</label>
-                        <input type="password"/>
-                    </div>
-                    <div class="input-div">
-                        <label class="input-label">비밀번호 확인</label>
-                        <input type="password"/>
-                    </div>
-                    <div class="input-div">
-                        <label class="input-label">이메일</label>
-                        <input type="text"/>
-                    </div>
-                    
+        <div class="container">
+            <div class="title">sign up</div>
+            <div class="flex">
+                <div class="input-div">
+                    <label class="input-label">아이디</label>
+                    <input type="text"/>
                 </div>
-                <button>
-                    next
-                </button>
+                <div class="input-div">
+                    <label class="input-label">비밀번호</label>
+                    <input type="password"/>
+                </div>
+                <div class="input-div">
+                    <label class="input-label">비밀번호 확인</label>
+                    <input type="password"/>
+                </div>
+                <div class="input-div">
+                    <label class="input-label">이메일</label>
+                    <input type="text"/>
+                </div>
+                    
             </div>
-        </body>
+            <button>
+                next
+            </button>
+        </div>
     )
 }
 
