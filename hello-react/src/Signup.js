44ec4efdadcadd0bb383f8ec036f1e51abfd0faf
Yu Jung Eun <0hh.0553@gmail.com>
2023-05-18 12:58:33 +0900

    media query 3단계

--- hello-react/src/Signup.js
+++ hello-react/src/Signup.js
@@ -10,19 +10,19 @@ function Signup() {
             <div class="flex">
                 <div class="input-div">
                     <label class="input-label">아이디</label>
-                    <input type="text"/>
+                    <input type="text" placeholder='ID'/>
                 </div>
                 <div class="input-div">
                     <label class="input-label">비밀번호</label>
-                    <input type="password"/>
+                    <input type="password" placeholder='Password'/>
                 </div>
                 <div class="input-div">
                     <label class="input-label">비밀번호 확인</label>
-                    <input type="password"/>
+                    <input type="password" placeholder='Password Again'/>
                 </div>
                 <div class="input-div">
                     <label class="input-label">이메일</label>
-                    <input type="text"/>
+                    <input type="text" placeholder="Email"/>
                 </div>
                     
             </div>
