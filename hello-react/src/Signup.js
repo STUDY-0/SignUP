3e1d7b6e9f8b25635d504d1cd8956df0a699de77
정은 유 <0hh.0553@gmail.com>
2023-05-20 10:56:06 +0900

    matchMedia를 사용해 placeholder를 js로 넣어줌

--- hello-react/src/Signup.js
+++ hello-react/src/Signup.js
@@ -4,25 +4,29 @@ import "./css/font.css"
 import "./css/base.css"
 
 function Signup() {
+    const script = document.createElement("script");
+    script.src = "./js/placeholder.js";
+    script.async = true;
+    document.body.appendChild(script);
     return (
         <div class="container">
             <div class="title">sign up</div>
             <div class="flex">
                 <div class="input-div">
-                    <label class="input-label">아이디</label>
-                    <input type="text" placeholder='ID'/>
+                    <label class="label">아이디</label>
+                    <input class='input' type="text" />
                 </div>
                 <div class="input-div">
-                    <label class="input-label">비밀번호</label>
-                    <input type="password" placeholder='Password'/>
+                    <label class="label">비밀번호</label>
+                    <input class='input' type="password" />
                 </div>
                 <div class="input-div">
-                    <label class="input-label">비밀번호 확인</label>
-                    <input type="password" placeholder='Password Again'/>
+                    <label class="label">비밀번호 확인</label>
+                    <input class='input' type="password" />
                 </div>
                 <div class="input-div">
-                    <label class="input-label">이메일</label>
-                    <input type="text" placeholder="Email"/>
+                    <label class="label">이메일</label>
+                    <input class='input' type="text" />
                 </div>
                     
             </div>
